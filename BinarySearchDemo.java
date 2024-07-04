37779429ed8a3ae81f025461e9161f895c881dc0
Jayarani .Y.S <jayaranilloyd1983@gmail.com>
2024-07-04 21:57:42 +0530

    Update BinarySearchDemo.java

--- BinarySearchDemo.java
+++ BinarySearchDemo.java
@@ -37,13 +37,3 @@ public class BinarySearchDemo {
     
 }
 
-//Bubble sort is a simple comparison-based sorting algorithm. It works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
-
-//Here's how bubble sort works:
-
-//Starting from the beginning of the list, compare the first two elements.
-//If the first element is greater than the second, swap them.
-//Move to the next pair of elements, and repeat step 2.
-//Continue this process for each pair of adjacent elements to the end of the list.
-//At the end of each pass through the list, the largest element will have "bubbled" up to its correct position at the end of the list.
-//Repeat the process for the remaining elements (excluding the last sorted elements) until no more swaps are needed.
