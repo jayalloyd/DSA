b3c25a3c3ffbbd782e02617aaf045d7d2d4bda41
jayalloyd <jayaranilloyd1983@gmail.com>
2024-11-13 21:59:56 +0530

    triangular pattern print

---
+++ PatternPrint1.java
@@ -0,0 +1,19 @@
+public class PatternPrint1 {
+    public static void main(String[] args) {
+       /*  int inumber of rows */
+    
+        /*star int j 
+        print
+*
+**
+***
+****    */
+        for (int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+                            
+            } 
+    
+    System.out.println();
+    }
+    }}
