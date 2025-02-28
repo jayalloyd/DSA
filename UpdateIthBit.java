fdddfc13772516c76a560d41d7fa692d1927874f
jayarani.ys <jayaraniys1@gmail.com>
2025-02-28 21:21:10 +0530

    some more added

--- UpdateIthBit.java
+++ UpdateIthBit.java
@@ -9,7 +9,19 @@ public class UpdateIthBit{
        int  bitMask=newBit<<i;
        return  bitMask | n;
     }
+    public static int clearLastIBits(int n,int i){
+        int bitMask=(~0)<<i;
+        return n& bitMask;
+    }
+    public static int clearRangeofBits(int n,int i,int j){
+        int a=(~0<<(j+1));
+        int b=(1<<i)-1;
+        int bitMask=a | b;
+        return n & bitMask;
+    }
     public static void main(String args[]){
-     System.out.print(updateItBitCode(10,2,1));
+    //  System.out.print(updateItBitCode(10,2,1));
+    //  System.out.println(clearLastIBits(15,2));
+    System.out.println(clearRangeofBits(10,2,4));
     }
 }
