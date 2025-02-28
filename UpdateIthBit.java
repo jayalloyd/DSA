a5e68d5fad9f2eb91563233fac50a55d3190ac48
jayarani.ys <jayaraniys1@gmail.com>
2025-02-28 20:49:06 +0530

    update Ith Bit code added

---
+++ UpdateIthBit.java
@@ -0,0 +1,15 @@
+public class UpdateIthBit{
+    public static int clearIthBitCode(int n,int i) {
+    int bitMask=~(1<<i);
+    return n& bitMask;
+   }
+   
+    public static int updateItBitCode(int n,int i,int newBit){
+       clearIthBitCode(n,i);
+       int  bitMask=newBit<<i;
+       return  bitMask | n;
+    }
+    public static void main(String args[]){
+     System.out.print(updateItBitCode(10,2,1));
+    }
+}
